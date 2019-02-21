197a6bc36130b0a584c409cbd3ed701c059cdc78
Pavel <r.p.1993@mail.ru>
2019-02-21 14:49:10 +0700

    add gulp,less,pug files

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    pug = require('gulp-pug'),
+    del = require('del');
+
+gulp.task('less',function (done) {
+    return gulp.src('dev/css/*.less',{base:'dev'})
+         //   .pipe(sourcemaps.init())
+            .pipe(autoprefixer())
+            .pipe(less())
+            //.pipe(concat('/styli/style.css'))
+           // .pipe(sourcemaps.write())
+            .pipe(gulp.dest('prod'));
+    done();
+});
+
+gulp.task('pug',function (done) {
+    return gulp.src('dev/*.pug',{base:'dev'})
+    //   .pipe(sourcemaps.init())
+       // .pipe(autoprefixer())
+        .pipe(pug({
+            pretty: true
+        }))
+        //.pipe(concat('/styli/style.css'))
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('prod'))
+    done();
+});
+
+gulp.task('clean', function(done) {
+     del.sync('prod');
+     done();
+});
+//
+//gulp.task('default', gulp.series('clean','less','pug'));
+
+
+gulp.task('build', gulp.series(
+    'clean',
+    gulp.parallel('less', 'pug'))
+);
+
+
+gulp.task('watch',function () {
+    gulp.watch('dev/css/*.less',gulp.series('less'));
+    gulp.watch('dev/*.pug',gulp.series('pug'));
+});
+
+gulp.task('dev',gulp.series('build','watch'));
+//gulp.build = series(clean, less);
